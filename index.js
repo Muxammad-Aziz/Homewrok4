1ed87977fe5e34a2145dc0fc98b835146e1343ae
Muxammad-Aziz <165257690+Muxammad-Aziz@users.noreply.github.com>
2024-04-09 17:50:27 +0500

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+// const arr = [1,2,3, 'Alex', ' I am', [1,2, 'Hello ' ], ' 24', 'My name is ', [1,[' years old', 'and']]]
+
+// let txt = arr[5] [2] + arr [7] + arr[3] + arr[4] + arr[6] + arr[8][1][0]
+
+// console.log(txt);
+
+
+
+const students = ['ilxom', 'xondamir', 'mirko', 'umar', 'ansor', 'muxammadAziz', 'sherobod', 'daler']
+
+
+let num = prompt('any num')
+if (num >= 0 && num <= 7) {
+    let mnb = +prompt('number')
+
+    if
+
+
+} else{
+    alert('alert')
+}
+// students.push = prompt('asd'('any nawwme'))
+
+
+console.log(students)
